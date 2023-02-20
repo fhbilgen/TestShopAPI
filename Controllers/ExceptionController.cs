8d85176032fa5a3ddd22d2acc155f969b1a613ef
Faik Hakan Bilgen <faikb@microsoft.com>
2023-02-20 21:33:58 +0300

    Added some problematic APIs

---
+++ Controllers/ExceptionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExceptionController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private static string notanumber = "abc";
+        public ExceptionController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet(Name = "GetException")]
+        public ActionResult<int> Get()
+        {
+            int i = int.Parse(notanumber);
+            return i;
+        }
+
+    }
+}
---
+++ Controllers/LongExecutionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LongExecutionController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+       
+        public LongExecutionController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetLongExecution")]
+        public ActionResult<int> Get() 
+        {
+            System.Threading.Thread.Sleep(50000);
+            return 50000;
+
+        }
+
+    }
+}
